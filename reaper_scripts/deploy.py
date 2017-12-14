ec362c5830ed42ea60188c1e5e70d1353f9a28d1
unknown <jaqms90@gmail.com>
2017-12-13 18:24:13 -0800

    Add deploy script

--- reaper_scripts/deploy.py
+++ reaper_scripts/deploy.py
@@ -3,23 +3,28 @@ import sys
 import shutil
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
-TOGGLE_FILENAME = 'Electribe - Toggle.lua'
-PAD_FILENAME = 'Electribe - Pad.lua'
-
-out_path = os.path.abspath(sys.argv[1])
-
-#create modal actions
-modals = [
+TOGGLE_FILE_NAME = 'Electribe - Toggle.lua'
+TOGGLE_FILE = SCRIPT_DIR + os.path.sep + TOGGLE_FILE_NAME
+PAD_FILE_NAME = 'Electribe - Pad.lua'
+PAD_FILE = SCRIPT_DIR + os.path.sep + PAD_FILE_NAME
+MODALS = [
     'edit_track_modal',
     'mute_track_modal',
     'erase_track_modal'
 ]
-for modal in modals:
-    out_filename = TOGGLE_FILENAME.replace('Toggle', 'Toggle ({0})'.format(modal))
-    shutil.copy2(SCRIPT_DIR + os.path.sep + TOGGLE_FILENAME, out_path + os.path.sep + out_filename)
+TRACKS = range(1, 16 + 1)
 
-#create track actions
-track_nums = range(1, 16 + 1)
-for track in track_nums:
-    out_filename = PAD_FILENAME.replace('Pad', 'Pad ({0})'.format(str(track)))
-    shutil.copy2(SCRIPT_DIR + os.path.sep + PAD_FILENAME, out_path + os.path.sep + out_filename)
+def run(out_dir):
+    #create modal actions
+    for modal in MODALS:
+        out_file_name = TOGGLE_FILE_NAME.replace('Toggle', 'Toggle ({0})'.format(modal))
+        out_file = out_dir + os.path.sep + out_file_name
+        shutil.copy2(TOGGLE_FILE, out_file)
+
+    #create track actions
+    for track in TRACKS:
+        out_file_name = PAD_FILE_NAME.replace('Pad', 'Pad ({0})'.format(str(track)))
+        out_file = out_dir + os.path.sep + out_file_name
+        shutil.copy2(PAD_FILE, out_file)
+
+run(os.path.abspath(sys.argv[1]))
